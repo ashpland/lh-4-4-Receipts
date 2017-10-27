3a49b1f3ae4e7f3648df2761b4cb36d4f8d28fc2
Andrew Shopland <hi@ashp.land>
2017-10-27 14:00:10 -0700

    add ui components

---
+++ Receipts/AddReceiptViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddReceiptViewController.h
+//  Receipts
+//
+//  Created by Andrew on 2017-10-27.
+//  Copyright © 2017 hearthedge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddReceiptViewController : UIViewController
+
+@end
---
+++ Receipts/AddReceiptViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddReceiptViewController.m
+//  Receipts
+//
+//  Created by Andrew on 2017-10-27.
+//  Copyright © 2017 hearthedge. All rights reserved.
+//
+
+#import "AddReceiptViewController.h"
+
+@interface AddReceiptViewController ()
+
+@end
+
+@implementation AddReceiptViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
