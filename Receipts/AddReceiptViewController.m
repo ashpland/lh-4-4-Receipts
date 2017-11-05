79f0be014dc84800ff7f2e52c224b370f73ff7eb
Andrew Shopland <hi@ashp.land>
2017-11-05 13:25:37 -0800

    Recipets get saved now

--- Receipts/AddReceiptViewController.m
+++ Receipts/AddReceiptViewController.m
@@ -70,6 +70,13 @@
                                             NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
                                             Tag *newTag = [[Tag alloc] initWithContext:context];
                                             newTag.tagName = newTagName;
+                                            NSError *error = nil;
+                                            if (![context save:&error]) {
+                                                // Replace this implementation with code to handle the error appropriately.
+                                                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                                                NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+                                                abort();
+                                            }
                                         }
                                      }];
     
